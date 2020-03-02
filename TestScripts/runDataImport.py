74098ba2447a096206bda0d1fb130a61b29a3055
tcmorgan2 <tcmorgan2@alaska.edu>
2020-03-02 13:18:36 -0900

    modified dictionary read to match current expected dictionary in fixBadData

--- TestScripts/runDataImport.py
+++ TestScripts/runDataImport.py
@@ -14,49 +14,63 @@ from MiGRIDS.InputHandler.DataClass import DataClass
 from MiGRIDS.InputHandler.fixBadData import fixBadData, fillComponentTypeLists
 from MiGRIDS.InputHandler.fixDataInterval import fixDataInterval
 from MiGRIDS.InputHandler.dataframe2netcdf import dataframe2netcdf
+from MiGRIDS.InputHandler.getSetupInformation import setupToDictionary, getSetupInformation
 from MiGRIDS.InputHandler.readData import readInputData_mp
 from MiGRIDS.InputHandler.readSetupFile import readSetupFile
 import pandas as pd
 
-#specify the correct path to your project setup file here
-fileName = os.path.join(os.getcwd(),*['..','MiGRIDSProjects','SampleProject','InputData','Setup','SampleProjectSetup.xml'])
-setupFolder = os.path.join(os.getcwd(),*['..','MiGRIDSProjects','SampleProject','InputData','Setup'])
-# get the setup Directory
 
-inputDictionary = readSetupFile(fileName)
+def modifyForManualInput(inputDictionary):
+    def keys_swap(orig_key, new_key, d):
+        d[new_key] = d.pop(orig_key)
+        return d
+
+    d = keys_swap('headerName.value', 'componentChannels.headerName.value', inputDictionary)
+    d = keys_swap('componentName.value', 'componentChannels.componentName.value', d)
+    d = keys_swap('componentAttribute.value', 'componentChannels.componentAttribute.value', d)
+    d = keys_swap('componentAttribute.unit', 'componentChannels.componentAttribute.unit', d)
+    return d
 
 
-# read time series data, combine with wind data if files are seperate.
-# updates are performed by the sender
+if __name__ == '__main__':
+    #specify the correct path to your project setup file here
+    fileName = os.path.join(os.getcwd(),*['..','MiGRIDSProjects','MyProject','InputData','Setup','MyProjectSetup.xml'])
+    setupFolder = os.path.join(os.getcwd(),*['..','MiGRIDSProjects','SampleProject','InputData','Setup'])
+    # get the setup Directory
 
-df, listOfComponents = readInputData_mp(inputDictionary)
+    soup = getSetupInformation(fileName)
+    inputDictionary = setupToDictionary(soup,fileName)
+    inputDictionary = modifyForManualInput(inputDictionary)
+    # read time series data, combine with wind data if files are seperate.
+    # updates are performed by the sender
 
-# check the timespan of the dataset. If its more than 1 year the dataset should have runTimesteps set
-minDate = min(df.index)
-maxDate = max(df.index)
+    df, listOfComponents = readInputData_mp(inputDictionary)
 
-# now fix the bad data
-# fixBad Data takes a DataClass object as input so create one
-# create DataClass object to store raw, fixed, and summery outputs
-data = DataClass(df, inputDictionary['runTimeSteps.value'])
-data.components = listOfComponents #list of component objects created during the import
-# parse data columns by type
-eColumns, loads, powerColumns = fillComponentTypeLists(listOfComponents) #assign the key data types in the dataset
-data.powerComponents = powerColumns
-data.ecolumns = eColumns
-data.loads = loads
+    # check the timespan of the dataset. If its more than 1 year the dataset should have runTimesteps set
+    minDate = min(df.index)
+    maxDate = max(df.index)
 
-data = fixBadData(data, setupFolder,
-                  inputDictionary['runTimeSteps.value'])
-#setup intervals
-data = fixDataInterval(data, pd.to_timedelta(inputDictionary['timestep.value'], unit=inputDictionary['timestep.unit']))
-data.preserve(setupFolder)
+    # now fix the bad data
+    # fixBad Data takes a DataClass object as input so create one
+    # create DataClass object to store raw, fixed, and summery outputs
+    data = DataClass(df, inputDictionary['runTimeSteps.value'])
+    data.components = listOfComponents #list of component objects created during the import
+    # parse data columns by type
+    eColumns, loads, powerColumns = fillComponentTypeLists(listOfComponents) #assign the key data types in the dataset
+    data.powerComponents = powerColumns
+    data.ecolumns = eColumns
+    data.loads = loads
 
-# now convert to a netcdf
-#components get passed as s dictionrary with their attributes
-componentDict = {}
-for c in listOfComponents:
-    componentDict[c.column_name] = c.toDictionary()
+    data = fixBadData(data, setupFolder,
+                      inputDictionary['runTimeSteps.value'])
+    #setup intervals
+    data = fixDataInterval(data, pd.to_timedelta(inputDictionary['timestep.value'], unit=inputDictionary['timestep.unit']))
+    data.preserve(setupFolder)
 
-dataframe2netcdf(data.fixed[0], componentDict,os.path.join(setupFolder,*['..','TimeSeriesData','ProcessedData'] ))
+    # now convert to a netcdf
+    #components get passed as s dictionrary with their attributes
+    componentDict = {}
+    for c in listOfComponents:
+        componentDict[c.column_name] = c.toDictionary()
 
+    dataframe2netcdf(data.fixed[0], componentDict,os.path.join(setupFolder,*['..','TimeSeriesData','ProcessedData'] ))
