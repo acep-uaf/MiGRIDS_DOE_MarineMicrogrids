2f4b6a959f6e2e184c58114b773f9695cfd07ee1
tcmorgan2 <tcmorgan2@alaska.edu>
2020-03-01 16:53:56 -0900

    run data import updated to reflect current data input process.

--- TestScripts/runDataImport.py
+++ TestScripts/runDataImport.py
@@ -1,6 +1,6 @@
-# Author: Jeremy VanderMeer, jbvandermeer@alaska.edu
-# Date: October 24, 2017
-# License: MIT License (see LICENSE file of this package for more information)
+# Author: T. Morgan
+# Date: January 19 2020
+
 
 # The goal of runDataImport is to take data from the user, check it for bad or missing data, and save it in a standard
 # netcdf format
@@ -8,59 +8,55 @@
 # this is run after the project files have been initiated (initiateProject.py) and filled (fillProjectData.py)
 
 # get input data to run the functions to import data into the project
-
+# each step should be run individually to evaluate expected outcomes
 import os
-from MiGRIDS.InputHandler.fixBadData import fixBadData
+from MiGRIDS.InputHandler.DataClass import DataClass
+from MiGRIDS.InputHandler.fixBadData import fixBadData, fillComponentTypeLists
 from MiGRIDS.InputHandler.fixDataInterval import fixDataInterval
 from MiGRIDS.InputHandler.dataframe2netcdf import dataframe2netcdf
-from MiGRIDS.InputHandler.mergeInputs import mergeInputs
+from MiGRIDS.InputHandler.readData import readInputData_mp
 from MiGRIDS.InputHandler.readSetupFile import readSetupFile
-import pickle
+import pandas as pd
 
 #specify the correct path to your project setup file here
 fileName = os.path.join(os.getcwd(),*['..','MiGRIDSProjects','SampleProject','InputData','Setup','SampleProjectSetup.xml'])
+setupFolder = os.path.join(os.getcwd(),*['..','MiGRIDSProjects','SampleProject','InputData','Setup'])
 # get the setup Directory
 
 inputDictionary = readSetupFile(fileName)
 
+
 # read time series data, combine with wind data if files are seperate.
-df, listOfComponents = mergeInputs(inputDictionary)
+# updates are performed by the sender
 
-#view the resulting dataframe -this will print to GUI console if open
-print(df.head())
+df, listOfComponents = readInputData_mp(inputDictionary)
 
-#save the dataframe and components used
-#its recommended that you save the data at each stage since the process can be time consuming to repeat.
-os.chdir(inputDictionary['setupDir'])
-out = open("df_raw.pkl","wb")
-pickle.dump(df,out )
-out.close()
-out = open("component.pkl","wb")
-pickle.dump(listOfComponents,out)
-out.close()
+# check the timespan of the dataset. If its more than 1 year the dataset should have runTimesteps set
+minDate = min(df.index)
+maxDate = max(df.index)
 
-#IF YOU ARE STARTING FROM AN EXISTING DF and COMPONENTS USE THE CODE BELOW TO LOAD
-os.chdir(inputDictionary['setupDir'])
-inFile = open("df_raw.pkl", "rb")
-df= pickle.load(inFile)
-inFile.close()
-inFile = open("component.pkl", "rb")
-listOfComponents = pickle.load(inFile)
-inFile.close()
+# now fix the bad data
+# fixBad Data takes a DataClass object as input so create one
+# create DataClass object to store raw, fixed, and summery outputs
+data = DataClass(df, inputDictionary['runTimeSteps.value'])
+data.components = listOfComponents #list of component objects created during the import
+# parse data columns by type
+eColumns, loads, powerColumns = fillComponentTypeLists(listOfComponents) #assign the key data types in the dataset
+data.powerComponents = powerColumns
+data.ecolumns = eColumns
+data.loads = loads
 
-#fix missing or bad data
-df_fixed = fixBadData(df, inputDictionary['setupDir'],listOfComponents,inputDictionary['runTimeSteps.value'])
-# fix the intervals
-df_fixed_interval = fixDataInterval(df_fixed,inputDictionary['timeSteps.value'])
-
-# pickle df
-os.chdir(inputDictionary['setupDir'])
-pickle.dump(df_fixed_interval, open("df_fixed_interval.p","wb"))
-
-d = {}
-for c in listOfComponents:
-    d[c.column_name] = c.toDictionary()
+data = fixBadData(data, setupFolder,
+                  inputDictionary['runTimeSteps.value'])
+#setup intervals
+data = fixDataInterval(data, pd.to_timedelta(inputDictionary['timestep.value'], unit=inputDictionary['timestep.unit']))
+data.preserve(setupFolder)
 
 # now convert to a netcdf
-dataframe2netcdf(df_fixed_interval.fixed[0], d,os.path.join(inputDictionary['setupDir'],*['..','TimeSeriesData','ProcessedData'] ))
+#components get passed as s dictionrary with their attributes
+componentDict = {}
+for c in listOfComponents:
+    componentDict[c.column_name] = c.toDictionary()
+
+dataframe2netcdf(data.fixed[0], componentDict,os.path.join(setupFolder,*['..','TimeSeriesData','ProcessedData'] ))
 
