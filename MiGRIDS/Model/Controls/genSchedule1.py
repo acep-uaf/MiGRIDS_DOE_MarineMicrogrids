3e98c3ca3f79ed7b9f831522b60e39411f1745f5
njgreen3 <njgreen3@alaska.edu>
2021-11-12 16:03:08 -0900

    Reordered time to switch calculations to be after selection of prefered gen combo. Only calculate time to switch for most prefered option.

--- MiGRIDS/Model/Controls/genSchedule1.py
+++ MiGRIDS/Model/Controls/genSchedule1.py
@@ -24,35 +24,6 @@ class genSchedule:
         ## first find all generator combinations that can supply the load within their operating bounds
         # find all with capacity over the load and the required SRC
         capReq = max(int(scheduledLoad - powerAvailToSwitch + scheduledSRCSwitch),0)
-        indCap = np.asarray(ph.lkpGenCombinationsUpperNormalLoading.get(capReq, ph.genCombinationsUpperNormalLoadingMaxIdx), dtype=int)
-
-        # check if the current online combination is capable of supplying the projected load minus the power available to
-        # help the current generator combination stay online
-        if ph.onlineCombinationID not in indCap and not (True in ph.outOfNormalBounds) and not underSRC: # keep current generating combingation in the mix unless has gone out of bounds for allotted amount
-                        #ph.genCombinationsUpperNormalLoading[ph.onlineCombinationID] >= scheduledLoad + scheduledSRCStay - powerAvailToStay:
-            # do not add the current generating option if it is diesel-off and it does not have enough SRC
-            #if not((ph.onlineCombinationID == 0) and underSRC):
-                indCap = np.append(indCap,ph.onlineCombinationID)
-        # if there are no gen combinations large enough to supply, automatically add largest (last combination)
-        if indCap.size == 0:
-            indCap = np.append(indCap,len(ph.genCombinationsUpperNormalLoading)-1)
-            indInBounds = indCap
-
-        elif indCap.size == 1:
-            indInBounds = indCap
-
-        else:
-            # find all with MOL under the load
-            indMOLCap = [idx for idx, x in enumerate(ph.genCombinationsMOL[indCap]) if x <= futureLoad]
-            # ind of in bounds combinations
-            indInBounds = indCap[indMOLCap]
-
-        # if there are no gen combinations with a low enough MOL enough to supply, automatically add combination 1,
-        # which is to smallest generator combination without turning off the generators
-        if indInBounds.size == 0:
-            indInBounds = np.array([indCap[0]])
-
-        indInBounds = np.atleast_1d(indInBounds)
 
         ## then check how long it will take to switch to any of the combinations online
         lenGenerators = len(ph.generators)
@@ -65,83 +36,71 @@ class genSchedule:
             # get the time remaining to turn off each generator
             turnOffTime[idx] = gen.genRunTimeMin - gen.genRunTimeAct - ph.timeStep
 
-        # Go through each potential combination of generators and find which generators need to be switched on and
-        # offline for each combination
-        lenIndInBounds = indInBounds.size #len(indInBounds)
-        genSwOn = [] # the generators to be switched on for each possible combination
-        genSwOff = [] # the generators to be switched off for each possible combination
-        timeToSwitch = [None]*lenIndInBounds # the time switch for each in bounds generator combination
-        # fuelCons = [None]*lenIndInBounds # the predicted fuel consumption for each combination
-        for ind, idx in enumerate(np.atleast_1d(indInBounds)): # for each combination that is in bounds
-            # inititiate the generators to be switched on for this combination to all generators in the combination
-            genSwOn.append(list(ph.genCombinationsID[idx]))
-            # initiate the generators to be switched off for this combination to all generators currently online
-            genSwOff.append(list(ph.genCombinationsID[ph.combinationsID.index(ph.onlineCombinationID)]))
-            # find common elements between switch on and switch off lists
-            commonGen = list(set(genSwOff[-1]).intersection(genSwOn[-1]))
-            # remove common generators from both lists
-            for genID in commonGen:
-                genSwOn[-1].remove(genID)
-                genSwOff[-1].remove(genID)
-            # for each gen to be switched get time, max time for combination is time will take to bring online
+        # find desired generation combination
+        if self.userDefinedGenList:
+            # single generator combination ID in a list (user defined)
+            indSort = ph.lkpUserDefinedGenSchedule.get(capReq, np.array([ph.genCombinationsUpperNormalLoadingMaxIdx]))
+                        
+        elif self.minimizeFuel:
+            # get the fuel consumption of possible gen combos at current capReq
+            # order gen combos by fuel consumption rate
+            fuelCons = ph.lkpMinFuelConsumption.get(capReq, np.array([0]))
+            fuelIDs = ph.lkpMinFuelConsumptionGenID.get(capReq, np.array([ph.genCombinationsUpperNormalLoadingMaxIdx]))
+            indSort = fuelIDs[np.argsort(fuelCons)]
+        else:
+            # get the MOL of possible gen combos at current capReq
+            # order gend combos by MOL
+            indCap = np.asarray(ph.lkpGenCombinationsUpperNormalLoading.get(capReq, ph.genCombinationsUpperNormalLoadingMaxIdx), dtype=int)
+            # check if the current online combination is capable of supplying the projected load minus the power available to
+            # help the current generator combination stay online
+            if ph.onlineCombinationID not in indCap and not (True in ph.outOfNormalBounds) and not underSRC: # keep current generating combingation in the mix unless has gone out of bounds for allotted amount
+                # do not add the current generating option if it is diesel-off and it does not have enough SRC
+                indCap = np.append(indCap,ph.onlineCombinationID)
+            indSort = indCap[np.argsort(ph.genCombinationsMOL[indCap])]
+        
+        # inititiate the generators to be switched on for this combination to all generators in the combination
+        genSwOn = list(ph.genCombinationsID[indSort[0]])
+        # initiate the generators to be switched off for this combination to all generators currently online
+        genSwOff = list(ph.genCombinationsID[ph.combinationsID.index(ph.onlineCombinationID)])
+        # find common elements between switch on and switch off lists
+        commonGen = list(set(genSwOff).intersection(genSwOn))
+        # remove common generators from both lists
+        for genID in commonGen:
+            genSwOn.remove(genID)
+            genSwOff.remove(genID)
+        # for each gen to be switched get time, max time for combination is time will take to bring online
 
-            # find max time to switch generators online
-            onTime = 0
-            for genID in genSwOn[-1]: # for each to be brought online in the current combination
-                onTime = max(onTime,turnOnTime[ph.genIDS.index(genID)]) # max turn on time
-            # find max of turn on time and turn off time
-            SwitchTime = onTime # initiate to max turn on time
-            for genID in genSwOff[-1]: # for each generator to be switched off in the current combination
-                SwitchTime = max(SwitchTime, turnOffTime[ph.genIDS.index(genID)]) # check if there is a higher turn off time
-            timeToSwitch[ind] = SwitchTime
-
-            # if self.minimizeFuel:
-            #     # get the generator fuel consumption at this loading for this combination
-            #     FCpower, FCcons = zip(*ph.genCombinationsFCurve[idx]) # separate out the consumptio n and power
-            #     # check if this is the online combination. If so, use the power available to stay online to calculate the
-            #     # the load required by the generator
-            #     if idx == ph.onlineCombinationID:
-            #         useScheduledLoad = int(max([scheduledLoad - powerAvailToStay, ph.genCombinationsMOL[idx]]))
-            #     else:
-            #         useScheduledLoad = int(max([scheduledLoad - powerAvailToSwitch, ph.genCombinationsMOL[idx]]))
-            #     indFCcons = getIntListIndex(useScheduledLoad, FCpower)
-
-            #     fuelCons[ind] = FCcons[indFCcons]
-            #     # TODO: Add cost of switching generators
-
-
-
+        # find max time to switch generators online
+        onTime = 0
+        for genID in genSwOn: # for each to be brought online in the current combination
+            onTime = max(onTime,turnOnTime[ph.genIDS.index(genID)]) # max turn on time
+        # find max of turn on time and turn off time
+        SwitchTime = onTime # initiate to max turn on time
+        for genID in genSwOff: # for each generator to be switched off in the current combination
+            SwitchTime = max(SwitchTime, turnOffTime[ph.genIDS.index(genID)]) # check if there is a higher turn off time
+        timeToSwitch = SwitchTime
+        
         ## bring the best option that can be switched immediatley, if any
         # if the most efficient option can't be switched, start warming up generators
-        # order fuel consumptions
-        if self.minimizeFuel:
-            # get the fuel consumption of possible gen combos at current capReq
-            fuelCons = ph.lkpMinFuelConsumption.get(capReq, ph.genCombinationsUpperNormalLoadingMaxIdx)
-            indSort = np.argsort(fuelCons)
-            # indSort = np.asarray(ph.lkpMinFuelConsumption.get(capReq, ph.genCombinationsUpperNormalLoadingMaxIdx), dtype=int)
-
-        else:
-            indSort = np.argsort(ph.genCombinationsMOL[indInBounds])
-
-        print(fuelCons.shape, ph.genCombinationsMOL[indInBounds].shape)
+        
         # if the most efficient can be switched on now, switch to it
-        if timeToSwitch[indSort[0]] <= 0:
+        if timeToSwitch <= 0:
             # update online generator combination
-            ph.onlineCombinationID = ph.combinationsID[indInBounds[indSort[0]]]
-            ph.switchGenComb(genSwOn[indSort[0]], genSwOff[indSort[0]])  # switch generators
+            ph.onlineCombinationID = ph.combinationsID[indSort[0]]
+            ph.switchGenComb(genSwOn, genSwOff)  # switch generators
             for idx in range(len(ph.genIDS)):
                 # update genPAvail
                 ph.generators[idx].updateGenPAvail()
         # otherwise, start or continue warming up generators for most efficient combination
         else:
-            ph.startGenComb(genSwOn[indSort[0]])
+            ph.startGenComb(genSwOn)
             # otherwise, if a generator is out of bounds (not just normal bounds) switch to the best possible, if can
-            if (True in (np.array(timeToSwitch)<=0)) & (True in ph.outOfBounds):
-                # find most efficient option that can be switched now
-                indBest = next((x for x in range(len(indSort)) if timeToSwitch[indSort[x]] <= 0 )) # indBest wrt indSort
-                # update online generator combination
-                ph.onlineCombinationID = ph.combinationsID[indInBounds[indSort[indBest]]]
-                ph.switchGenComb(genSwOn[indSort[indBest]],genSwOff[indSort[indBest]]) # switch generators
-                for idx in range(len(ph.genIDS)):
-                    # update genPAvail
-                    ph.generators[idx].updateGenPAvail()
+            # if (True in (np.array(timeToSwitch)<=0)) & (True in ph.outOfBounds):
+            #     # find most efficient option that can be switched now
+            #     indBest = next((x for x in range(len(indSort)) if timeToSwitch[indSort[x]] <= 0 )) # indBest wrt indSort
+            #     # update online generator combination
+            #     ph.onlineCombinationID = ph.combinationsID[indInBounds[indSort[indBest]]]
+            #     ph.switchGenComb(genSwOn[indSort[indBest]],genSwOff[indSort[indBest]]) # switch generators
+            #     for idx in range(len(ph.genIDS)):
+            #         # update genPAvail
+            #         ph.generators[idx].updateGenPAvail()
