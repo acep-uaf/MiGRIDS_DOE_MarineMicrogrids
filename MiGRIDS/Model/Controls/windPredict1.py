3309cbe24ab669df9413bf3ce98de4e608ca596b
jbvandermeer <jbvandermeer@alaska.edu>
2022-03-08 09:59:05 -0900

    Add a near term wind prediction

--- MiGRIDS/Model/Controls/windPredict1.py
+++ MiGRIDS/Model/Controls/windPredict1.py
@@ -16,11 +16,13 @@ class windPredict:
         # prevWindProfile is a list of lists of previous wind power profiles, or a list of estimates of previous wind power
         # simple calculation, return the mean of the last 5 min load
         self.futureWind = []
+        self.futureWindNearTerm = []
         #startIdx10min = max(SO.idx - int(600/SO.timeStep),0)
         #stopIdx = SO.idx + 1
         # for each wind turnbine
         for wtg in SO.WF.windTurbines:
             # the future power possible is calculated as the average possible power in the past 10 min.
             fw10min = wtg.windPower10minTrend[SO.masterIdx] #np.min(wtg.windPower[startIdx10min:stopIdx])
+            fw10sec = wtg.windPower10sTrend[SO.masterIdx]
             self.futureWind += [fw10min]
-
+            self.futureWindNearTerm += [fw10sec]
