53e29365db46ea7659854a2b5a339da2e11d5c3e
jbvandermeer <jbvandermeer@alaska.edu>
2022-03-08 10:00:28 -0900

    created a perfect wind predictor controller. Instead of other wind predict functions that estimate future wind based on previous wind.

---
+++ MiGRIDS/Model/Controls/windPredict2.py
@@ -0,0 +1,24 @@
+# Project: MiGRIDS
+# Author: Jeremy VanderMeer, jbvandermeer@alaska.edu
+# Date: January 19, 2022
+# License: MIT License (see LICENSE file of this package for more information)
+
+# imports
+import numpy as np
+
+# calculate a short term future Wind power
+class windPredict:
+
+    def __init__(self,args):
+        self.futureWind = [0]
+        self.std = args['std']
+        self.nearTermWindow = args['nearTermWindow']
+    def windPredict(self, SO):
+        self.futureWind = []
+        self.futureWindNearTerm = []
+        #startIdx10min = max(SO.idx - int(600/SO.timeStep),0)
+        #stopIdx = SO.idx + 1
+        # for each wind turnbine
+        for wtg in SO.WF.windTurbines:
+            self.futureWind += [wtg.windPower10minTrend[SO.masterIdx]]
+            self.futureWindNearTerm += [min(wtg.windPower[SO.masterIdx:int(SO.masterIdx+self.nearTermWindow/SO.timeStep)])]
