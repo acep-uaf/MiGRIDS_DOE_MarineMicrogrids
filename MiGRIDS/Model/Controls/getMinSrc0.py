c35d44cf8d8107610cc56ecd076fcaace8ba7f7e
jbvandermeer <jbvandermeer@alaska.edu>
2022-03-08 09:58:02 -0900

    add a near term SRC requirement

--- MiGRIDS/Model/Controls/getMinSrc0.py
+++ MiGRIDS/Model/Controls/getMinSrc0.py
@@ -15,20 +15,24 @@ class getMinSrc:
         self.minSrcToStay = 0
         self.minSrcToSwitch = 0
 
-    def getMinSrc(self, SO, calcFuture = False):
+    def getMinSrc(self, SO, calcFuture = False, nearTerm = False):
 
         # Take the average load of this hour last week 24.5hr * 3600 s/hr = 617400s , 23.5hr * 3600 = 592200
         # try with and without, compare the difference in performance.
         #meanLastWeek = np.mean(prevLoad[int(-617400/timeStep):int(-592200/timeStep)])
         # if a list is given for prevLoad, get mean value, otherwise, use the value given
 
-        if calcFuture:
+        if calcFuture and not nearTerm:
             # the minimum src required.
             # remove non firm power from the SRC requirements
             self.minSrcToStay = SO.futureLoad * SO.DM.minSRC + sum([a * b for a, b in zip(SO.futureWind, SO.WF.wtgMinSrcCover)])  # catch zero instances of wind power with max statement. in this case, nonFirmWtgP will be 0 as well.
             # when scheduling new units online, use a higher SRC in order to avoid having just enough power to cover SRC required
             # and then have the requirement increase
             self.minSrcToSwitch = self.minSrcToStay * self.minSRCToSwitchRatio
+        elif calcFuture and nearTerm:
+            self.minSrcToStay = SO.futureLoad * SO.DM.minSRC + sum([a * b for a, b in zip(SO.futureWindNearTerm, SO.WF.wtgMinSrcCover)])  # catch zero instances of wind power with max statement. in this case, nonFirmWtgP will be 0 as well.
+            self.minSrcToSwitch = self.minSrcToStay * self.minSRCToSwitchRatio
+
         else:
 
             meanLastTenMin = SO.DM.realLoad10minTrend[SO.masterIdx] #np.mean(SO.DM.realLoad[max((SO.idx - int(600/SO.timeStep)), 0):(SO.idx+1)])
